8106c906ded771e513b9821b26ba42e3a78b7e24
Sourec <tothemyers@gmail.com>
2015-05-25 15:45:50 -0400

    Absorbables are now drawn from the center outward
    Previously absorbables were drawn from the top right corner outward, but
    no longer! Note that this is only a graphical change, so collision
    detection should still work.

--- src/Absorbable.cpp
+++ src/Absorbable.cpp
@@ -29,8 +29,10 @@ void Absorbable::draw(SDL_Renderer* renderer, float vPosX, float vPosY, float sc
 	//Draw a circle... actually a square for now. Sorry!
 	double radius = mass * scale; //TODO: Ensure that this formula works as intended...
 	SDL_Rect rect;
-	rect.x = posX - (vPosX * scale), rect.y = posY - (vPosY * scale);
-	rect.h = radius, rect.w = radius;
+	rect.x = posX - (vPosX * scale) - (radius / 2.f);
+	rect.y = posY - (vPosY * scale) - (radius / 2.f);
+	rect.h = radius,
+			rect.w = radius;
 	SDL_RenderFillRect(renderer, &rect);
 }
 void Absorbable::setMass(float newMass)
